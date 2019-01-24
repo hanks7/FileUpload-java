062c5a0e3f11f9a7cedaf9c7bcada93d9815169f
hjj <474664736@qq.com>
2019-01-24 16:48:28 +0800

    Initial commit

---
+++ src/com/imooc/servlet/InitServlet.java
@@ -0,0 +1,26 @@
+package com.imooc.servlet;
+
+import com.imooc.domain.User;
+import com.imooc.utils.Ulog;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet(name = "InitServlet")
+public class InitServlet extends HttpServlet {
+    @Override
+    public void init() throws ServletException {
+
+        List<User> list = new ArrayList<>();
+        getServletContext().setAttribute("list",list);
+        Ulog.i("开始了");
+
+    }
+
+}
